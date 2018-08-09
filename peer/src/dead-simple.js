ddfcbf3b4dd7396c14b4b82ce1a79620a1d15722
Alessandro Mariotti <a.mariotti@themadbox.com>
2018-08-09 10:40:58 +0200

    dead simple

---
+++ peer/src/dead-simple.js
@@ -0,0 +1,76 @@
+let localVideo;
+let remoteVideo;
+let peerConnection;
+let peerConnectionConfig = {
+    iceServers: [{ 'url': 'stun:stun.services.mozilla.com' }, { 'url': 'stun:stun.l.google.com:19302' }]
+};
+let serverConnection;
+let localStream;
+
+function start(isCaller) {
+    peerConnection = new RTCPeerConnection(peerConnectionConfig);
+    peerConnection.onicecandidate = gotIceCandidate;
+    peerConnection.onaddstream = gotRemoteStream;
+    peerConnection.addStream(localStream);
+
+    if (isCaller) {
+        peerConnection.createOffer(gotDescription, createOfferError);
+    }
+}
+
+function gotDescription(description) {
+    console.log('got description');
+    peerConnection.setLocalDescription(description, function() {
+        serverConnection.send(JSON.stringify({ 'sdp': description }));
+    }, function() { console.log('set description error') });
+}
+
+function gotIceCandidate(event) {
+    if (event.candidate != null) {
+        serverConnection.send(JSON.stringify({ 'ice': event.candidate }));
+    }
+}
+
+function gotRemoteStream(event) {
+    console.log('got remote stream');
+    remoteVideo.src = window.URL.createObjectURL(event.stream);
+}
+
+function createOfferError(error) {
+    console.log(error);
+}
+
+function gotMessageFromServer(message) {
+    if (!peerConnection) start(false);
+
+    var signal = JSON.parse(message.data);
+    if (signal.sdp) {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp), function() {
+            if (signal.sdp.type == 'offer') {
+                peerConnection.createAnswer(gotDescription, createAnswerError);
+            }
+        });
+    } else if (signal.ice) {
+        peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice));
+    }
+}
+
+function pageReady() {
+    localVideo = document.getElementById('local-video');
+    remoteVideo = document.getElementById('remote-video');
+
+    serverConnection = new WebSocket('ws://192.168.10.56:8080');
+    serverConnection.onmessage = gotMessageFromServer;
+
+    var constraints = {
+        video: true,
+        audio: true,
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints, stream => {
+        localStream = stream;
+        localVideo.srcObject = stream;
+    }, error => console.warn(error));
+}
+
+pageReady();
