dd229ed90d16ba4cb563dbac9a5b67ad674abe20
Alessandro Mariotti <a.mariotti@themadbox.com>
2018-08-09 22:14:45 +0200

    working

---
+++ p2p-video-chat-webrtc-electron/src/index.js
@@ -0,0 +1,45 @@
+function main(initiator) {
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+    }).then(stream => {
+        const Peer = require('simple-peer');
+        const config = {
+            initiator: initiator,
+            trickle: false,
+            stream: stream
+        };
+        console.log(config);
+        const peer = new Peer(config);
+
+        peer.on('signal', data => {
+            document.getElementById('yourId').value = JSON.stringify(data);
+        });
+
+        peer.on('data', data => {
+            document.getElementById('messages').textContent += data + '\n';
+        });
+
+        peer.on('stream', stream => {
+            let video = document.createElement('video');
+            document.body.appendChild(video);
+            video.srcObject = stream;
+            video.play();
+        });
+
+        document.getElementById('connect').addEventListener('click', () => {
+            let otherId = document.getElementById('otherId').value;
+            otherId = JSON.parse(otherId);
+            peer.signal(otherId);
+        });
+
+        document.getElementById('send').addEventListener('click', () => {
+            let yourMessage = document.getElementById('yourMessage').value;
+            peer.send(yourMessage);
+        });
+    }).catch(error => console.log(error));
+}
+
+document.getElementById('start').addEventListener('click', () => {
+    main(true);
+});
